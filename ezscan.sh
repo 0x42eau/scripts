a51766d202eec31580dd2edf49fdfffbd6751e63
0x42eau <49952735+0x42eau@users.noreply.github.com>
2024-03-08 13:02:34 -0700

    Update ezscan.sh

--- ezscan.sh
+++ ezscan.sh
@@ -4,7 +4,7 @@
 # ./ezscan.sh scope.file scan-rate
 # ./ezscan.sh scope.txt 4000
 
-masscan -p21,22,23,25,53,80,110,111,135-139,143,443,445,502,993,995,1433,1434,1723,3306,3389,5900,8000,8080 -iL $1 --rate $2 -oB masscan.mass
+masscan -p21,22,23,25,53,80,88,110,111,135-139,143,389,443,445,502,636,993,995,1433,1434,1723,3306,3389,5900,5985,5986,8000,8080,8443 -iL $1 --rate $2 -oB masscan.mass
 
 masscan --open --readscan masscan.mass > masscan.grep
 
@@ -15,15 +15,19 @@ cat masscan.grep | grep -i '22/tcp' | awk -F " " '{print $6}' > ssh.hosts
 cat masscan.grep | grep -i '23/tcp' | awk -F " " '{print $6}' > telnet.hosts
 cat masscan.grep | grep -i '25/tcp' | awk -F " " '{print $6}' > smtp.hosts
 cat masscan.grep | grep -i '80/tcp' | awk -F " " '{print $6}' > http.hosts
+cat masscan.grep | grep -i '88/tcp' | awk -F " " '{print $6}' > kerberos.hosts
 cat masscan.grep | grep -i '443/tcp' | awk -F " " '{print $6}' > https.hosts
+cat masscan.grep | grep -iE '389/tcp|636/tcp' | awk -F " " '{print $6}' > ldap.hosts
 cat masscan.grep | grep -i '8000/tcp' | awk -F " " '{print $6}' > http-8000.hosts
 cat masscan.grep | grep -i '8080/tcp' | awk -F " " '{print $6}' > http-8080.hosts
+cat masscan.grep | grep -i '8443/tcp' | awk -F " " '{print $6}' > http-8443.hosts
 cat masscan.grep | grep -i '53/tcp' | awk -F " " '{print $6}' > dns.hosts
 cat masscan.grep | grep -i '445/tcp' | awk -F " " '{print $6}' > smb.hosts
 cat masscan.grep | grep -i '1433/tcp' | awk -F " " '{print $6}' > mssql.hosts
 cat masscan.grep | grep -i '3306/tcp' | awk -F " " '{print $6}' > sql.hosts
 cat masscan.grep | grep -i '3389/tcp' | awk -F " " '{print $6}' > rdp.hosts
 cat masscan.grep | grep -i '5900/tcp' | awk -F " " '{print $6}' > vnc.hosts
+cat masscan.grep | grep -iE '5985/tcp|5986/tcp' | awk -F " " '{print $6}' > winrm.hosts
 cat masscan.grep | grep -i '502/tcp' | awk -F " " '{print $6}' > modbus.hosts
 
 #for gowitness script
