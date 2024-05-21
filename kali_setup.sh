b4acaecb3b867614906c431d7fab2ce764158df3
0x42eau <49952735+0x42eau@users.noreply.github.com>
2024-05-21 09:36:37 -0600

    Create kali_setup.sh

---
+++ kali_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# option "N" for new install, make root login
+git clone https://github.com/Dewalt-arch/pimpmykali.git /opt
+/opt/pimpmykali/pimpmykali.sh
+
+git clone https://github.com/0x42eau/scripts.git /opt
+
+chmod +x /opt/scripts/*
+
+/opt/scripts/command_logging.sh
