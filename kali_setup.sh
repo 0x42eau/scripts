4b655c091d7ddecfb5a6aaaf21c9517bde2b4fb3
0x42eau <49952735+0x42eau@users.noreply.github.com>
2024-06-18 12:58:54 -0600

    Update kali_setup.sh

--- kali_setup.sh
+++ kali_setup.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
 
-# option "N" for new install, make root login
+# option "N" for new install, no make root login
+echo '###################'
+echo '[*] Downloading and running pimpmykali, for'
+echo '###################'
 git clone https://github.com/Dewalt-arch/pimpmykali.git /opt
-/opt/pimpmykali/pimpmykali.sh
+echo "N" | /opt/pimpmykali/pimpmykali.sh
 
+echo '###################'
+echo '[*] grabbing my scripts and putting into /opt'
+echo '###################'
 git clone https://github.com/0x42eau/scripts.git /opt
 
 chmod +x /opt/scripts/*
 
+echo '###################'
+echo '[*] turning on logging'
+echo '###################'
 /opt/scripts/command_logging.sh
+
+echo '###################'
+echo '[*] mapping pi-hole for dns'
+echo '###################'
 /opt/add-pihole-nameserver.sh
+
+echo '###################'
+echo '[*] installing brave'
+echo '###################'
 /opt/install_brave.sh
+
+
+echo '###################'
+echo '[*] Running pimpmykali again to set up root account'
+echo '###################'
+sleep 5s
+/opt/pimpmykali/pimpmykali.sh
