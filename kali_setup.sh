55fb0b23554b6083f0642a2616187b27b5162ba3
0x42eau <49952735+0x42eau@users.noreply.github.com>
2024-06-24 09:00:26 -0600

    Update kali_setup.sh

--- kali_setup.sh
+++ kali_setup.sh
@@ -5,6 +5,45 @@ apt update && apt upgrade -y
 # install vmware tools 
 sudo apt-get install open-vm-tools -y
 
+# add vmware shared folders 
+# MY ENV/PREFS ONLY
+# Define the mount point
+# checking for kali repos in /etc/apt/sources.list and adding if snot
+echo '###################'
+echo '[*] Adding sharefolder to /mnt/hgfs'
+echo '###################'
+
+MOUNT_POINT="/mnt/hgfs"
+
+# Check if the mount point already exists
+if mountpoint -q "$MOUNT_POINT"; then
+    echo "$MOUNT_POINT is already mounted."
+    exit 0
+fi
+
+# Try the first command
+if /usr/bin/vmhgfs-fuse .host:/ $MOUNT_POINT -o subtype=vmhgfs-fuse,allow_other; then
+    echo "Mounted using /usr/bin/vmhgfs-fuse .host:/ $MOUNT_POINT -o subtype=vmhgfs-fuse,allow_other"
+    exit 0
+fi
+
+# Try the second command
+if mount -t vmhgfs .host:F:/VMShared /mnt/vmshared; then
+    echo "Mounted using mount -t vmhgfs .host:F:/VMShared /mnt/vmshared"
+    exit 0
+fi
+
+# Try the third command
+if /usr/bin/vmhgfs-fuse .host:F:/VMShared /mnt/vmshared -o subtype=vmhgfs-fuse,allow_other; then
+    echo "Mounted using /usr/bin/vmhgfs-fuse .host:F:/VMShared /mnt/vmshared -o subtype=vmhgfs-fuse,allow_other"
+    exit 0
+fi
+
+# Try the fourth command
+if mount -t fuse.vmhgfs-fuse .host:/ $MOUNT_POINT -o allow_other; then
+    echo "Mounted using mount -t fuse.vmhgfs-fuse .host:/ $MOUNT_POINT -o allow_other"
+    exit 0
+fi
 
 # option "N" for new install, no make root login
 echo '###################'
